f9ba4ddb089447d6aa68dda1e3b01aebc192f75f
Éder Porto <eder.porto@hotmail.com>
2023-03-12 04:16:47 -0300

    updating settings.py

--- sara/settings.py
+++ sara/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'Teste'
+SECRET_KEY = os.environ.get('django_secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['sara-wmb.toolforge.org', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['sara-wmb.toolforge.org']
 
 # Application definition
 
