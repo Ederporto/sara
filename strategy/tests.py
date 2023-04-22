3b31203c9ac52017f432fae4885bc52914952ee5
Éder Porto <eder.porto@hotmail.com>
2023-04-22 04:49:37 -0300

    strategy tests

--- strategy/tests.py
+++ strategy/tests.py
@@ -1,132 +1,43 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
 from .models import StrategicAxis, Direction
-
+from django.urls import reverse
 
 class DirectionModelTests(TestCase):
-    def test_direction_without_english_text_shows_that_text(self):
-        """
-        Objects without text in English are created normally and its string is this text
-        """
-        text = "Direction"
-        strategic_axis, created = StrategicAxis.objects.get_or_create(text="StrategicAxis")
+    def setUp(self):
+        self.strategic_axis = StrategicAxis.objects.create(text="Strategic Axis")
 
-        direction = Direction.objects.create(text=text, strategic_axis=strategic_axis)
+    def test_direction_str_returns_its_text(self):
+        direction = Direction.objects.create(text="Direction", strategic_axis=self.strategic_axis)
+        self.assertEqual(str(direction), "Direction")
 
-        direction.clean()
-        self.assertEqual(direction.text, direction.__str__())
-        self.assertIsNone(direction.text_en)
-
-    def test_direction_with_only_english_text_raises_validation_error(self):
-        """
-        When objects don't have the text field filled, the object is not created
-        """
-        text_en = "Direction_English"
-        strategic_axis, created = StrategicAxis.objects.get_or_create(text="StrategicAxis")
-
-        direction = Direction.objects.create(text_en=text_en, strategic_axis=strategic_axis)
-
-        try:
-            direction.clean()
-        except ValidationError as e:
-            self.assertTrue("You need to fill the text field" in e.message)
-
-    def test_direction_with_both_texts_shows_the_non_english_text(self):
-        """
-        Objects with both texts filled give preference to the non english (local)
-        language
-        """
-        text = "Direction"
-        text_en = "Direction_English"
-        strategic_axis, created = StrategicAxis.objects.get_or_create(text="StrategicAxis")
-
-        direction = Direction.objects.create(text=text, text_en=text_en, strategic_axis=strategic_axis)
-        direction.clean()
-
-        self.assertEqual(direction.text, direction.__str__())
-        self.assertNotEqual(direction.text_en, direction.__str__())
-
-    def test_create_direction_without_any_text_raises_validation_error(self):
-        """
-        Objects without any text are not created and a validation error is raised
-        """
-        strategic_axis, created = StrategicAxis.objects.get_or_create(text="StrategicAxis")
-        direction = Direction.objects.create(strategic_axis=strategic_axis)
-
-        try:
-            direction.clean()
-        except ValidationError as e:
-            self.assertTrue('You need to fill the text field' in e.message)
-
-    def test_create_direction_with_text_is_indeed_created(self):
-        """
-        Objects with the proper parameters are created
-        """
-        number_of_directions_on_database_before_creation = Direction.objects.count()
-        text = "Direction"
-        text_en = "Direction_English"
-        strategic_axis, created = StrategicAxis.objects.get_or_create(text="StrategicAxis")
-        direction = Direction.objects.create(text=text, text_en=text_en, strategic_axis=strategic_axis)
-        direction.clean()
-
-        number_of_directions_on_database_after_creation = Direction.objects.count()
-        self.assertNotEqual(number_of_directions_on_database_before_creation,
-                            number_of_directions_on_database_after_creation)
+    def test_direction_cannot_have_empty_text(self):
+        with self.assertRaises(ValidationError):
+            direction = Direction(text="", strategic_axis=self.strategic_axis)
+            direction.full_clean()
 
 
 class StrategicAxisModelTests(TestCase):
-    def test_strategic_axis_without_english_text_shows_that_text(self):
-        """
-        Objects without text in English are created normally and its string is this text
-        """
-        text = "Strategic Axis"
-        strategic_axis = StrategicAxis.objects.create(text=text)
-        self.assertEqual(strategic_axis.text, strategic_axis.__str__())
-        self.assertIsNone(strategic_axis.text_en)
+    def setUp(self):
+        self.strategic_axis = StrategicAxis.objects.create(text="Strategic Axis")
 
-    def test_strategic_axis_with_only_english_text_raises_validation_error(self):
-        """
-        When objects do not have the text field filled, the object is not created
-        """
-        text_en = "Strategic Axis"
-        strategic_axis = StrategicAxis.objects.create(text_en=text_en)
-        self.assertNotEqual(strategic_axis.text_en, strategic_axis.__str__())
-        self.assertEqual(strategic_axis.text, '')
+    def test_strategic_axis_str_returns_its_text(self):
+        self.assertEqual(str(self.strategic_axis), "Strategic Axis")
 
-    def test_strategic_axis_with_both_texts_shows_the_non_english_text(self):
-        """
-        Objects with both texts filled give preference to the non english (local)
-        language
-        """
-        text = "Strategic Axis"
-        text_en = "Strategic Axis_English"
-        strategic_axis = StrategicAxis.objects.create(text=text, text_en=text_en)
+    def test_strategic_axis_cannot_have_empty_text(self):
+        with self.assertRaises(ValidationError):
+            axis = StrategicAxis(text="")
+            axis.full_clean()
 
-        self.assertEqual(strategic_axis.text, strategic_axis.__str__())
-        self.assertNotEqual(strategic_axis.text_en, strategic_axis.__str__())
-        self.assertIsNotNone(strategic_axis.text_en)
-        self.assertIsNotNone(strategic_axis.text)
 
-    def test_create_strategic_axis_without_any_text_raises_validation_error(self):
-        """
-        Objects without any text are not created and a validation error is raised
-        """
-        strategic_axis = StrategicAxis.objects.create()
+class StrategicAxisViewTests(TestCase):
+    def setUp(self):
+        self.strategic_axis1 = StrategicAxis.objects.create(text="Strategic Axis 1")
+        self.strategic_axis2 = StrategicAxis.objects.create(text="Strategic Axis 2")
 
-        try:
-            strategic_axis.clean()
-        except ValidationError as e:
-            print(e.message)
-            self.assertTrue('You need to fill the text field' in e.message)
-
-    def test_create_strategic_axis_with_text_is_indeed_created(self):
-        """
-        Objects with the proper parameters are created
-        """
-        number_of_strategic_axiss_on_database_before_creation = StrategicAxis.objects.count()
-        text = "Test"
-        strategic_axis = StrategicAxis.objects.create(text=text)
-        number_of_strategic_axiss_on_database_after_creation = StrategicAxis.objects.count()
-        self.assertIsNotNone(strategic_axis.text)
-        self.assertNotEqual(number_of_strategic_axiss_on_database_before_creation,
-                            number_of_strategic_axiss_on_database_after_creation)
+    def test_show_strategy_view(self):
+        response = self.client.get(reverse("strategy:show_strategy"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "metrics/strategy.html")
+        self.assertContains(response, self.strategic_axis1.text)
+        self.assertContains(response, self.strategic_axis2.text)
--- strategy/views.py
+++ strategy/views.py
@@ -3,11 +3,5 @@ from .models import StrategicAxis
 
 
 def show_strategy(request):
-    """
-    View of the strategic axis and respective action points
-    :param request:
-    :return: rendering template strategy.html which shows a list of
-    """
-
     strategic_axes = StrategicAxis.objects.all()
     return render(request, "metrics/strategy.html", {"axes": strategic_axes})
