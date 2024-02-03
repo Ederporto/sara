02824243feba6281b75686ca4f042b1a0b74af39
Éder Porto <eder.porto@hotmail.com>
2024-02-02 23:51:49 -0300

    detail_report update and fixing

--- report/views.py
+++ report/views.py
@@ -136,7 +136,12 @@ def list_reports(request):
 @login_required
 @permission_required("report.view_report")
 def detail_report(request, report_id):
-    context = {"data": Report.objects.get(id=report_id),
+    report = Report.objects.get(id=report_id)
+    operations = OperationReport.objects.filter(report=report)
+    operations_with_value =operations.filter(Q(number_of_people_reached_through_social_media__gt=0) | Q(number_of_new_followers__gt=0) | Q(number_of_mentions__gt=0) | Q(number_of_community_communications__gt=0) | Q(number_of_events__gt=0) | Q(number_of_resources__gt=0) | Q(number_of_partnerships_activated__gt=0) | Q(number_of_new_partnerships__gt=0)).exists()
+    context = {"data": report,
+               "operations": OperationReport.objects.filter(report=report),
+               "operations_with_value": operations_with_value,
                "title": _("View report %(report_id)s") % {"report_id": report_id}}
 
     return render(request, "report/detail_report.html", context)
