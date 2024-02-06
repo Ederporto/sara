96469b999ef208fec341023f61a6d38f34a063d5
Éder Porto <eder.porto@hotmail.com>
2024-02-05 21:39:12 -0300

    Reviewing admin fields

--- metrics/admin.py
+++ metrics/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Objective, Area, Activity, Metric, Project
 
 admin.site.register(Project)
-admin.site.register(Objective)
 admin.site.register(Area)
+admin.site.register(Objective)
 admin.site.register(Activity)
 admin.site.register(Metric)
--- report/admin.py
+++ report/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-from .models import EvaluationObjective, Report, AreaActivated, Editor, Funding, \
-    LearningArea, Organizer, Partner, Technology, StrategicLearningQuestion
+from .models import EvaluationObjective, Report, AreaActivated, Editor, Funding, LearningArea, Organizer, Partner, \
+    Technology, StrategicLearningQuestion, OperationReport
 
+admin.site.register(Funding)
+admin.site.register(Editor)
+admin.site.register(Partner)
+admin.site.register(Organizer)
+admin.site.register(Technology)
+admin.site.register(AreaActivated)
+admin.site.register(LearningArea)
+admin.site.register(StrategicLearningQuestion)
 admin.site.register(EvaluationObjective)
 admin.site.register(Report)
-admin.site.register(Editor)
-admin.site.register(Funding)
-admin.site.register(LearningArea)
-admin.site.register(Organizer)
-admin.site.register(Partner)
-admin.site.register(Technology)
-admin.site.register(StrategicLearningQuestion)
-admin.site.register(AreaActivated)
+admin.site.register(OperationReport)
