17bece4e37bf4bd9225e27ba964f9e55a96e3f83
Éder Porto <eder.porto@hotmail.com>
2025-02-19 00:00:55 -0300

    Removing strategy view; Changing navbar

--- strategy/views.py
+++ strategy/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
-from .models import StrategicAxis
-from django.utils.translation import gettext as _
-
+from django.shortcuts import redirect
+from django.conf import settings
 
 def show_strategy(request):
-    strategic_axes = StrategicAxis.objects.all().order_by("id")
-    context = {"axes": strategic_axes, "title": _("Strategy")}
-    return render(request, "metrics/strategy.html", context)
+    return redirect(settings.STRATEGY_URL)
